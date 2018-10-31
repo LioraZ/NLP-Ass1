9c779b481ebb99cc3662fda8c48456b601606838
LioraZ <liorazr@gmail.com>
2018-10-31 18:26:22 +0200

    part1 ready- creates estimation and quantities folder

--- MLETrain.py
+++ MLETrain.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from itertools import islice
+import sys
 
 
 def reading_input(fname):
@@ -9,7 +10,9 @@ def reading_input(fname):
         for word in data:
             item = word.split('/')
             tags.append(item[-1])
-    return tags
+        for i in range(len(data)):
+            data[i] = data[i].replace("/", " ")
+    return tags, data
 
 
 def counting_quantities(tags):
@@ -18,8 +21,14 @@ def counting_quantities(tags):
     return Counter(pairs), Counter(triplets)
 
 
-def write_quantities(pairs, triplets):
-    with open("q.mle", "w") as file:
+def write_estimations(pairs, tags, f_name):
+    with open(f_name, "w") as file:
+        for i in pairs:
+            file.write(i + '\t' + str(pairs[i]) + '\n' + str(i.split(" ")[1]) + '\t' + str(tags[i.split(" ")[1]]) + '\n')
+
+
+def write_quantities(pairs, triplets, f_name):
+    with open(f_name, "w") as file:
         for key in triplets:
             pair_key = " ".join([k for k in key.split()[:-1]])
             file.write(str(key) + '\t' + str(triplets[key]) + '\n')
@@ -40,5 +49,8 @@ def combinations(seq, n=2):
 
 
 if __name__ == '__main__':
-    pairs, triplets = counting_quantities(reading_input("data/ass1-tagger-train"))
-    write_quantities(pairs, triplets)
+    script_name, f_name, q_mle, e_mle = sys.argv
+    tags, data = reading_input(f_name)
+    write_estimations(Counter(data), Counter(tags), e_mle)
+    pairs, triplets = counting_quantities(tags)
+    write_quantities(pairs, triplets, q_mle)
